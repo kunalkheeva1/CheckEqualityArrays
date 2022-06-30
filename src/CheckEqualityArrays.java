4dd16473550547296a8321f5377f1ad083db75de
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-29 20:56:56 -0700

    created a method to check if two arrays are equal or not, the condition is they are unsorted and contain duplicates too. so i managed runtime On and space complexity is On too

--- src/CheckEqualityArrays.java
+++ src/CheckEqualityArrays.java
@@ -1,5 +1,33 @@
+import java.util.HashMap;
+
 public class CheckEqualityArrays {
+
+    public static boolean checkEqualityArrays(int arr1 [], int arr2[], int n){
+        //map
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //add all the elements of arr1 in map
+        for(int i= 0; i<n; i++){
+            map.put(arr1[i], map.getOrDefault(arr1[i],0)+1);
+        }
+        //if map contain any elemnent of arr2
+        for(int i=0; i<n; i++){
+            if(map.containsKey(arr2[i])){
+                //then put
+                map.put(arr2[i], map.get(arr2[i]) -1 );
+                if(map.get(arr2[i])<0){
+                    return false;
+                }
+                else{
+                    return false;
+                }
+            }
+        }return true;
+
+
+
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
